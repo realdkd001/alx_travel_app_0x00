aff70d0c46ec0e80fa12da83113108489c7e6d30
Dzrekey001 <dkdzrekey001@gmail.com>
2025-07-18 21:59:41 +0000

    listings/serializers.py file exists and not empty

--- alx_travel_app/alx_travel_app/listings/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({"message": "Welcome to the Listings API!"})
