406a2fdd129c3d8ae1bb050ee69f7d24f514c219
Dzrekey001 <dkdzrekey001@gmail.com>
2025-08-03 11:36:01 +0000

    Initial commit

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -33,6 +33,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', include('listings.urls')),  # Your app's URLs
+    path('api/', include('listings.urls')),
 ]
 
--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
-from django.http import JsonResponse
 
-def home(request):
-    return JsonResponse({"message": "Welcome to the Listings API!"})
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
